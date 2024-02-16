00a1631dff71525590bf562182fcd40661c99bc9
Vilas Balu Rabad <rabadvilas11@gmail.com>
2024-02-16 14:43:36 +0530

    add logout controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -107,10 +107,33 @@ const loginUser = asyncHandler(async(req, res)=>{
 })
 
 
+const logoutUser = asyncHandler(async(req, res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
 
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "Logout user Successfully!"));
+})
 
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { logoutUser, loginUser, registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+
+//secure routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
